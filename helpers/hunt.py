601778169a685283ff411c34043b0de4c1b9ce7e
Overtime <Truen2005@gmail.com>
2023-02-05 21:20:37 -0500

    stuff

--- helpers/hunt.py
+++ helpers/hunt.py
@@ -1,16 +1,19 @@
-import aiosqlite
-import requests
-import aiohttp
-import discord
-from typing import Tuple, Any, Optional, Union
+import asyncio
+import json
+import os
 import random
+from io import BytesIO
+from typing import Any, Optional, Tuple, Union
 
+import aiohttp
+import aiosqlite
+import discord
+import requests
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
-from helpers import db_manager
 from PIL import Image, ImageChops, ImageDraw, ImageFont
-import os
-import asyncio
-from io import BytesIO
-import json
-        
+
+from helpers import db_manager, battle
+
+async def hunt(ctx: Context):
+    pass
