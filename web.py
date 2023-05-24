5425cb5c6d2f0e27e0a382ba586fa612348962bd
Overtime <Truen2005@gmail.com>
2023-05-24 19:33:48 -0400

    Update web.py

--- web.py
+++ web.py
@@ -7,28 +7,37 @@ import discord
 from helpers import db_manager
 from itsdangerous import URLSafeTimedSerializer
 import uvicorn
+import os
+import sys
+import json
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key='your secret key')  # replace with your secret key
 
 templates = Jinja2Templates(directory="templates")
 
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
+
 @app.get("/")
-def index(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/thanks")
-def thanks(request: Request):
+async def thanks(request: Request):
     return templates.TemplateResponse("thanks.html", {"request": request})
 
 @app.get("/webhook")
-def webhook(request: Request):
+async def webhook(request: Request):
     discord_user_id = request.query_params.get("discord_id")
     request.session["discord_user_id"] = discord_user_id
     return RedirectResponse(url="https://id.twitch.tv/oauth2/authorize?client_id=xulcmh65kzbfefzuvfuulnh7hzrfhj&redirect_uri=https://dankstreamer.lol/callback&response_type=code&scope=user:read:email")
 
 @app.get("/callback")
-def callback(request: Request):
+async def callback(request: Request):
     code = request.query_params.get("code")
     client_id = "xulcmh65kzbfefzuvfuulnh7hzrfhj"
     client_secret = "fsbndw0gusm5lzvqnz7v5d59x34n94"
@@ -51,20 +60,22 @@ def callback(request: Request):
     discord_id = request.session["discord_user_id"]
     broadcaster_type = user["broadcaster_type"]
 
-    db_manager.edit_twitchCreds(access_token)  
-    db_manager.connect_twitch_id(discord_id, user['id'])  
-    db_manager.connect_twitch_name(discord_id, user['login'])  
+    await db_manager.edit_twitchCreds(access_token)  
+    await db_manager.connect_twitch_id(discord_id, user['id'])  
+    await db_manager.connect_twitch_name(discord_id, user['login'])  
 
     if broadcaster_type in ["affiliate", "partner"]:
         emotePrefix = user['login'][:4]
-        db_manager.add_streamer(user['login'], discord_id, emotePrefix, user['id'], broadcaster_type)
+        await db_manager.add_streamer(user['login'], discord_id, emotePrefix, user['id'], broadcaster_type)
         embed_description = "Your accounts have been connected! You have been registered as a streamer. Your generated prefix is: " + emotePrefix
     else:
         embed_description = "Your accounts have been connected!"
 
     embed = discord.Embed(title="Connected!", description=embed_description, color=0x00ff00)
     embed.set_footer(text=f"Discord ID: {discord_id} | Twitch ID: {user['id']}")
-    client = discord.Client()
+    intents = discord.Intents.all()
+    client = discord.Client(intents)
+    client.start(config['token'])
     client.get_user(int(discord_id)).send(embed=embed)
     print(f"Connected {discord_id} to {user['id']}!")
     client.close()
