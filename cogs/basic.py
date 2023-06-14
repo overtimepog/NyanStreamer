4b6b2bf9572dd06c19c29371ca8c6d0098348d40
Overtime <Truen2005@gmail.com>
2023-06-13 22:55:04 -0400

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -688,7 +688,7 @@ class Basic(commands.Cog, name="basic"):
                 resetTime = resetTime.strftime('%B %d, %Y, %I:%M %p %Z')
                 shop_embed = discord.Embed(
                     title="Shop",
-                    description=f"This is the shop, you can buy items here with `/buy itemid #` EX. `/buy iron_sword 1`. \n \n Shop Resets: `{resetTime}`"
+                    description=f"This is the shop here you can buy items \n Commands: \n `/buy itemid #` EX. `/buy iron_sword 1`. \n \n Shop Resets: `{resetTime}` \n"
                 )
                 shop_embed.set_footer(text=f"Page {i + 1}/{num_pages}")
 
