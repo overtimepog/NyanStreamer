83486f73762416618105e5f785ae642b61afa4f0
Overtime <Truen2005@gmail.com>
2023-06-19 12:52:28 -0400

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -1135,7 +1135,7 @@ class Basic(commands.Cog, name="basic"):
 
         timed_items = await db_manager.view_timed_items(user.id)
         
-        est_min_datetime = datetime.datetime.min.replace(tzinfo=pytz.UTC).astimezone(pytz.timezone('US/Eastern'))
+        est_min_datetime = datetime.datetime(1970, 1, 1, tzinfo=pytz.UTC).astimezone(pytz.timezone('US/Eastern'))
         item_info = defaultdict(lambda: {"count": 0, "latest_expire": est_min_datetime})
 
         
