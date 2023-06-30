889484c545ebdd0668ac87bcb1782b7dc305d0ec
Overtime <Truen2005@gmail.com>
2023-06-29 20:21:23 -0400

    Update basic.py

--- cogs/basic.py
+++ cogs/basic.py
@@ -1019,8 +1019,8 @@ class Basic(commands.Cog, name="basic"):
         net_dict = await bank.get_user_net_worth(user)
         embed.add_field(name="Net Worth", value=f'{cash}{int(net_dict["networth"]):,}', inline=True)
         #add xp and level
-        embed.add_field(name="XP", value=f"{user_xp} / {xp_needed}", inline=False)
-        embed.add_field(name="Level", value=f"{user_level}", inline=False)
+        embed.add_field(name="XP", value=f"{user_xp} / {xp_needed}", inline=True)
+        embed.add_field(name="Level", value=f"{user_level}", inline=True)
         #get the badges from the database
         badges = await db_manager.get_equipped_badges(user_id)
         #make a feild for the badges and set the title to badges and the value to the badges
