4bb44219b93cc0cef2a286343c7847cd1dabf09a
Overtime <Truen2005@gmail.com>
2023-07-25 06:22:07 -0400

    Update draw.py

--- drawing_code/cogs/Draw/draw.py
+++ drawing_code/cogs/Draw/draw.py
@@ -1766,7 +1766,7 @@ class Draw(commands.Cog, name="Draw"):
         name="copy",
         brief="Copy a drawing.",
         help="Copy a drawing from an embed by replying to the message or using message link.",
-        description="Allows you to copy a drawing that was done with the `draw` command. This will also copy the palette! You can copy by replying to such a message or by providing the message link (or ID).",
+        description="Allows you to copy a drawing that was done with the `draw` command.",
     )
     async def copy(
         self,
