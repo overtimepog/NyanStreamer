893b8b27dadf05423207b66585d5ae2dabab7755
Overtime <Truen2005@gmail.com>
2023-08-13 23:28:56 -0400

    fixed

--- assets/endpoints/beemovielawyer.py
+++ assets/endpoints/beemovielawyer.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont, ImageSequence
+from assets.utils import http
 from io import BytesIO
 
 class BeeMovieLawyer:
@@ -6,43 +7,64 @@ class BeeMovieLawyer:
     This endpoint returns a GIF with text placed at specific coordinates.
     Ensure your application can handle the GIF format.
     Malformed requests count against your ratelimit for this endpoint.
-    Dynamically place the text on the GIF within the coordinates starting at -1,2 and ending at 719,240.
+    Place text on the GIF at the specified coordinates.
     """
     params = ['text']
 
     def generate(self, avatars, text, usernames, kwargs):
-        # Load the GIF from the specified path
+        # Load the GIF
         gif = Image.open('assets/assets/beemovielawyer/beemovielawyer.gif')
         
-        # Define the font for the text using the provided path
-        font_size = 30
-        font = ImageFont.truetype('assets/assets/fonts/Arial-Bold.ttf', font_size)
-        
-        # Calculate text width and height
-        text_width, text_height = ImageDraw.Draw(gif).multiline_textsize(text, font=font)
-        
-        # Adjust font size if text width is greater than the specified width
-        while text_width > 720:
-            font_size -= 1
-            font = ImageFont.truetype('assets/assets/fonts/Arial-Bold.ttf', font_size)
-            text_width, text_height = ImageDraw.Draw(gif).multiline_textsize(text, font=font)
-        
-        # Calculate position to center the text
-        x_pos = (-1 + 719 - text_width) // 2
-        y_pos = (2 + 240 - text_height) // 2
-        
+        # Create a drawing context
         frames = []
-        for frame in ImageSequence.Iterator(gif):
-            # Create a drawing context
-            draw = ImageDraw.Draw(frame)
-            
-            # Place the text at the calculated coordinates
-            draw.text((x_pos, y_pos), text, font=font, fill="white")
-            
-            # Append the frame to the frames list
-            frames.append(frame.copy())
+        font_path = 'assets/assets/fonts/impact.ttf'
+        max_width = 639
+        max_height = 208
+        initial_font_size = 40
+        font = ImageFont.truetype(font_path, initial_font_size)
+
+        # Reduce font size until the text width is less than max_width
+        while font.getsize(text)[0] > max_width:
+            initial_font_size -= 1
+            font = ImageFont.truetype(font_path, initial_font_size)
+
+        # Wrap text if it's too long to fit on one line
+        wrapped_text = ""
+        for word in text.split():
+            if font.getsize(wrapped_text + word)[0] <= max_width:
+                wrapped_text += word + " "
+            else:
+                wrapped_text += "\n" + word + " "
         
-        # Convert the frames to GIF and return
+        # Split the wrapped text into lines
+        lines = wrapped_text.split("\n")
+        
+        # Ensure the total height of the text block doesn't exceed max_height
+        while len(lines) * font.getsize(lines[0])[1] > max_height:
+            initial_font_size -= 1
+            font = ImageFont.truetype(font_path, initial_font_size)
+            wrapped_text = ""
+            for word in text.split():
+                if font.getsize(wrapped_text + word)[0] <= max_width:
+                    wrapped_text += word + " "
+                else:
+                    wrapped_text += "\n" + word + " "
+            lines = wrapped_text.split("\n")
+
+        # Calculate the starting y-coordinate to center the text vertically
+        y_start = (max_height - len(lines) * font.getsize(lines[0])[1]) // 2
+
+        for frame in ImageSequence.Iterator(gif):
+            d = ImageDraw.Draw(frame)
+            y = y_start
+            for line in lines:
+                width, height = d.textsize(line, font=font)
+                # Draw text centered in the specified rectangle
+                d.text(((max_width - width) / 2, y), line, font=font, fill="white")
+                y += height
+            frames.append(frame)
+
+        # Convert the frames to bytes and return
         b = BytesIO()
         frames[0].save(b, format='GIF', append_images=frames[1:], save_all=True, duration=gif.info['duration'], loop=0)
         b.seek(0)
