fdc2cd2c59d2ba91b32b260983b3fa62b6e13d13
Overtime <Truen2005@gmail.com>
2023-08-30 14:51:55 -0400

    Update images.py

--- cogs/images.py
+++ cogs/images.py
@@ -34,7 +34,7 @@ import subprocess
 import fcntl
 client = JeyyAPIClient('6COJCCHO74OJ2CPM6GRJ4C9O6OS3G.9PSM2RH0ADQ74PB1DLIN4.FOauZ8Gi-J7wAuWDj_hH-g')
     # Define a function that wraps around the spinning_model function
-Nyan_Api_Key = 'WHNmMVu_FQi8o5WwJOy_-klIwEJN-EXHdRbqXKfhYvQ'
+Nyan_Api_Key = 'b7CE_rDmkHupVy65Ni55vXX_FOYx2lRjSR8RHRtAXVs'
 
 
 def format_text(text):
