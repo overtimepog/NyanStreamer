00533738c840ff994c2045b03dd7c9e01d2cb388
Overtime <Truen2005@gmail.com>
2023-09-17 16:48:08 -0400

    Update suffering.py

--- assets/endpoints/suffering.py
+++ assets/endpoints/suffering.py
@@ -9,7 +9,9 @@ class Suffering():
         base = Image.open('assets/assets/suffering/suffering.png').convert('RGBA')
         
         # Get the avatar image and resize it to fit the coordinates
-        avatar = Image.open(avatars[0]).resize((1227-650, 849-265)).convert('RGBA')
+        response = requests.get(avatars[0])
+        avatar_data = BytesIO(response.content)
+        avatar = Image.open(avatar_data).resize((1227-650, 849-265)).convert('RGBA')
         
         # Paste the avatar image onto the main image at the specified coordinates
         base.paste(avatar, (650, 265), avatar)
